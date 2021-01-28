768e83be5c186b6ad368617bf45a1cb4fcf9d7b0
Salatiel dos Reis <salatieldrg@gmail.com>
2021-01-28 20:42:41 -0300

    Crud cliente com jdbc

--- src/main/java/io/github/salatieldosreisgomes/VendasApplication.java
+++ src/main/java/io/github/salatieldosreisgomes/VendasApplication.java
@@ -12,6 +12,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @SpringBootApplication
 @RestController
 public class VendasApplication {
@@ -25,9 +27,35 @@ public class VendasApplication {
     @Bean
     public CommandLineRunner init(@Autowired Clientes clientes){
         return args -> {
-            Cliente cliente = new Cliente();
-            cliente.setNome("Salatiel");
-            clientes.salvar(cliente);
+            System.out.println("Salvando clientes");
+            clientes.salvar(new Cliente("Salatiel"));
+            clientes.salvar(new Cliente("Outro Cliente"));
+
+            List<Cliente> todosClientes = clientes.obterTodos();
+            todosClientes.forEach(System.out::println);
+
+            System.out.println("Atualizando clientes");
+            todosClientes.forEach(c -> {
+                c.setNome(c.getNome() + " Atualizado");
+                clientes.atualizar(c);
+            });
+
+            System.out.println("Buscando clientes");
+            clientes.buscarPorNome("Cli").forEach(System.out::println);
+
+            /*System.out.println("Deletando clientes");
+            clientes.obterTodos().forEach(c -> {
+                clientes.deletar(c);
+            });
+             */
+
+            todosClientes = clientes.obterTodos();
+            if (todosClientes.isEmpty()){
+                System.out.println("Nenhum cliente cadastrado");
+            }else {
+                todosClientes.forEach(System.out::println);
+            }
+
         };
     }
 
--- src/main/java/io/github/salatieldosreisgomes/domain/repositorio/Clientes.java
+++ src/main/java/io/github/salatieldosreisgomes/domain/repositorio/Clientes.java
@@ -3,12 +3,23 @@ package io.github.salatieldosreisgomes.domain.repositorio;
 import io.github.salatieldosreisgomes.domain.entity.Cliente;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
 
 @Repository
 public class Clientes {
 
+
+
     private static String INSERT = "insert into cliente (nome) values (?) ";
+    private static String SELECT_ALL = "SELECT * FROM CLIENTE ";
+    private static String UPDATE = "update cliente set nome = ? where id = ? ";
+    private static String DELETE = "delete from cliente where id = ? ";
+    private static String SELECT_POR_NOME = "select * from cliente where nome = ? ";
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
@@ -16,5 +27,41 @@ public class Clientes {
     public Cliente salvar(Cliente cliente){
         jdbcTemplate.update(INSERT, new Object[]{cliente.getNome()});
         return cliente;
+    }
+
+    public Cliente atualizar(Cliente cliente){
+        jdbcTemplate.update(UPDATE, new Object[]{
+                cliente.getNome(), cliente.getId()
+        });
+        return cliente;
+    }
+
+    public void deletar(Cliente cliente){
+        deletar(cliente.getId());
+    }
+
+    public void deletar(Integer id){
+        jdbcTemplate.update(DELETE, new Object[]{id});
+    }
+
+    public List<Cliente> buscarPorNome(String nome){
+        return jdbcTemplate.query(SELECT_ALL.concat(" where nome like ? "),
+                new Object[]{"%" + nome + "%"},
+                obterClienteMapper());
+    }
+
+    public List<Cliente> obterTodos(){
+        return jdbcTemplate.query(SELECT_ALL, obterClienteMapper());
+    }
+
+    private RowMapper<Cliente> obterClienteMapper() {
+        return new RowMapper<Cliente>() {
+            @Override
+            public Cliente mapRow(ResultSet resultSet, int i) throws SQLException {
+                Integer id = resultSet.getInt("id");
+                String nome = resultSet.getString("nome");
+                return new Cliente(id, nome);
+            }
+        };
     }
 }
